c7f3c8ab1dd6c9de51a234d20d985182a10f9d16
Bryce Roney <bryceroney@users.noreply.github.com>
2021-10-12 16:21:15 +1100

    Allow read_absmap to handle uppercase names (#57)
    Add uppercase name support to read_absmap

--- tests/testthat/test-read_absmaps.R
+++ tests/testthat/test-read_absmaps.R
@@ -1,6 +1,7 @@
 test_that("read_absmaps() works", {
 
   expect_type(read_absmap("sa42016"), "list")
+  expect_type(read_absmap("SA42016"), "list")
 
   expect_identical(names(read_absmap("state2021")),
                    c("state_code_2021",
@@ -9,6 +10,12 @@ test_that("read_absmaps() works", {
                      "cent_lat",
                      "cent_long",
                      "geometry"))
+
+  expect_identical(names(read_absmap("SA32016")),
+                   c("sa3_code_2016", "sa3_name_2016", "sa4_code_2016",
+                     "sa4_name_2016", "gcc_code_2016", "gcc_name_2016",
+                     "state_code_2016", "state_name_2016", "areasqkm_2016", "cent_long",
+                     "cent_lat", "geometry"))
 
   # errors
   expect_error(read_absmap(), regexp = "Please")
