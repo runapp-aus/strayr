a397b7e3b3f7f62455a32753426e2fcf67e97957
Peter Owen <pete.owen1@hotmail.co.uk>
2023-08-15 14:32:15 +1000

    update anzsic2006 to download from abs source

--- data-raw/create_anzsic2006.R
+++ data-raw/create_anzsic2006.R
@@ -2,47 +2,78 @@
 # Reading and cleaning ANZSIC correspondence
 
 library(tidyverse)
-library(glue)
+library(rvest)
 
 # include factor variants or nah?
 include_factor_variants <- FALSE
 
-# ty asiripanich
-anzsic_url <- "https://raw.githubusercontent.com/asiripanich/anzsic/master/anzsic_2006.csv"
+# fetch from abs website
+url <- "https://www.abs.gov.au/statistics/classifications/australian-and-new-zealand-standard-industrial-classification-anzsic/2006-revision-2-0/numbering-system-and-titles/division-subdivision-group-and-class-codes-and-titles"
 
-# Read
-anzsic_raw <- read_csv(anzsic_url) %>%
-    rename_all(~ glue("anzsic_{.}")) %>%
-    mutate_if(is.double, as.integer) %>%
-    as_tibble()
+df <- url %>%
+  rvest::read_html() %>%
+  rvest::html_table()
 
-# Add layers of nfd
-class_nfd <- anzsic_raw %>%
-  distinct(anzsic_division_title, anzsic_division_code,
-           anzsic_subdivision_title, anzsic_subdivision_code,
-           anzsic_group_title, anzsic_group_code) %>%
-  mutate(anzsic_class_code = anzsic_group_code * 10,
-         anzsic_class_title = glue("{anzsic_group_title}, nfd"))
+# bind tables together
+anzsic_2006_temp <-
+  purrr::list_rbind(df)
 
-group_nfd <- anzsic_raw %>%
-  distinct(anzsic_division_title, anzsic_division_code,
-           anzsic_subdivision_title, anzsic_subdivision_code) %>%
-  mutate(anzsic_group_title = glue("{anzsic_subdivision_title}, nfd"),
-         anzsic_group_code = anzsic_subdivision_code * 10,
-         anzsic_class_title = anzsic_group_title,
-         anzsic_class_code = anzsic_group_code * 10)
+# fix columns names and bind together
+colnames(anzsic_2006_temp) <- c("anzsic_division_code", "anzsic_subdivision_code", "anzsic_group_code", "anzsic_class_code", "title")
 
-subdivision_nfd <- anzsic_raw %>%
-  group_by(anzsic_division_code, anzsic_division_title) %>%
-  summarise(anzsic_subdivision_code = min(anzsic_subdivision_code)) %>%
-  mutate(anzsic_subdivision_title = glue("{anzsic_division_title}, nfd"),
-         anzsic_group_title = anzsic_subdivision_title,
-         anzsic_group_code = anzsic_subdivision_code * 10,
-         anzsic_class_title = anzsic_group_title,
-         anzsic_class_code = anzsic_group_code * 10)
+first_row <-
+  as.data.frame(t(colnames(df[[1]])))
+
+colnames(first_row) <- c("anzsic_division_code", "anzsic_subdivision_code", "anzsic_group_code", "anzsic_class_code", "title")
+
+anzsic_2006_total <-
+  dplyr::bind_rows(first_row, anzsic_2006_temp)
+
+# replace blanks with NAs
+anzsic_2006_total[anzsic_2006_total == ""] <- NA
+
+# fill NAs down from above
+anzsic_2006_fill <-
+  anzsic_2006_total %>%
+  tidyr::fill(colnames(anzsic_2006_total), .direction = c("down"))
+
+# get each grouping type individually
+anzsic_2006_class <-
+  anzsic_2006_total %>%
+  dplyr::filter(stringr::str_detect(anzsic_class_code, "^[:digit:]+$")) %>%
+  dplyr::select(anzsic_class_code, anzsic_class_title = title)
+
+anzsic_2006_group <-
+  anzsic_2006_total %>%
+  dplyr::filter(stringr::str_detect(anzsic_group_code, "^[:digit:]+$")) %>%
+  dplyr::select(anzsic_group_code, anzsic_group_title = title)
+
+anzsic_2006_subdivision <-
+  anzsic_2006_total %>%
+  dplyr::filter(stringr::str_detect(anzsic_subdivision_code, "^[:digit:]+$")) %>%
+  dplyr::select(anzsic_subdivision_code, anzsic_subdivision_title = title)
+
+anzsic_2006_division <-
+  anzsic_2006_total %>%
+  dplyr::filter(stringr::str_detect(anzsic_division_code, "^[:alpha:]+$")) %>%
+  dplyr::select(anzsic_division_code, anzsic_division_title = title)
+
+# combine grouping types into final table
+anzsic_2006_final <-
+  anzsic_2006_fill %>%
+  dplyr::left_join(anzsic_2006_division) %>%
+  dplyr::left_join(anzsic_2006_subdivision) %>%
+  dplyr::left_join(anzsic_2006_group) %>%
+  dplyr::left_join(anzsic_2006_class) %>%
+  dplyr::filter(!is.na(anzsic_class_title)) %>%
+  dplyr::select(
+    anzsic_division_code, anzsic_division_title, anzsic_subdivision_code, anzsic_subdivision_title,
+    anzsic_group_code, anzsic_group_title, anzsic_class_code, anzsic_class_title
+  ) %>%
+  dplyr::as_tibble()
 
 # Finalise data frame; noting that we are avoiding the nfd complication for now
-anzsic2006 <- anzsic_raw %>%
+anzsic2006 <- anzsic_2006_final %>%
   arrange(anzsic_division_code,
           anzsic_subdivision_code,
           anzsic_group_code,
