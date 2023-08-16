4327c257af23ff11541ac37042d0492a9ea7babd
Peter Owen <pete.owen1@hotmail.co.uk>
2023-08-16 15:57:31 +1000

    consider '-' as missing value in spreadsheets

--- R/seifa.R
+++ R/seifa.R
@@ -261,7 +261,7 @@ get_seifa_index_sheet <- function(filename, sheetname, structure = c("sa1", "sa2
       sheetname,
       skip = 6,
       col_names = column_names,
-      na = c("", "NA")
+      na = c("", "NA",'-')
     ) %>%
       dplyr::filter(across(ends_with("_code"), ~ !is.na(.x))) %>%
       select(-starts_with("blank")) %>%
